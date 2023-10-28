90810a8d21523b581454f6a292269218dd08b5e7
JuanC <133279376+JuanKld117@users.noreply.github.com>
2023-10-27 19:02:10 -0600

    Update BatallaPokemon.java

--- LenguajeDeProgamación2/Pokemon_Polimorfismo/BatallaPokemon.java
+++ LenguajeDeProgamación2/Pokemon_Polimorfismo/BatallaPokemon.java
@@ -3,7 +3,6 @@ import pokemon.*;
 public class BatallaPokemon {
     public static void iniciarBatalla(Pokemon Charizard, Pokemon Venusaur) {
         System.out.println("Batalla entre " + Charizard.getNombre() + " y " + Venusaur.getNombre());
-
         while((Charizard.getHp() > 0 && Venusaur.getHp() > 0)) {
             realizarAtaque(Charizard, 0, Venusaur);
             realizarAtaque(Venusaur, 1, Charizard);
@@ -11,12 +10,13 @@ public class BatallaPokemon {
         determinarGanador(Charizard, Venusaur);
     }
 
+    
     public static void realizarAtaque(Pokemon atacante, int movimiento, Pokemon defensor) {
         atacante.atacar(movimiento, defensor);
         System.out.println(atacante.getNombre() + " HP = " + atacante.getHp());
     }
 
-
+    
     public static void determinarGanador(Pokemon Charizard, Pokemon Venusaur) {
         if (Charizard.getHp() <= 0) {
             System.out.println(Charizard.getNombre() + " ha perdido la batalla.");
@@ -24,6 +24,8 @@ public class BatallaPokemon {
             System.out.println(Venusaur.getNombre() + " ha perdido la batalla.");
         }
     }
+
+    
     public static void main(String[] args) {
         Pokemon charizard = new Charizard("Charizard");
         Pokemon venusaur = new Venusaur("Venusaur");
