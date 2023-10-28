e00291dcf8208c6fac10ff17749d7df4b6deb95f
JuanC <133279376+JuanKld117@users.noreply.github.com>
2023-10-27 19:06:38 -0600

    Update PokemonInterface.java

--- LenguajeDeProgamación2/Pokemon_Polimorfismo/pokemon/PokemonInterface.java
+++ LenguajeDeProgamación2/Pokemon_Polimorfismo/pokemon/PokemonInterface.java
@@ -1,9 +1,8 @@
 package pokemon;
-
 import pokemon.movimientos.Movimientos;
+
 
 public interface PokemonInterface{
     public boolean seHaConcretadoAtaque(Pokemon pokemon, Movimientos ataque);
     public double obtenerBonificacion(Pokemon pokemon, Movimientos ataque);
-
 }
